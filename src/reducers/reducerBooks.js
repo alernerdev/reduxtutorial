2a72995772f46c10b4d0a221534b07d419d45452
Alex Lerner <lernera@thomsonreuters.com>
2018-07-26 16:40:16 -0400

    initial commit of redux tutorial managing a list of books and a active book

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: "Javascript"},
+        {title: "Harry Potter"},
+        {title: "Hello Dolly"},
+        {title: "Dark Tower"},
+        {title: "Python"},
+    ];
+}
